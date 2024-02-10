1f93b226d8c9f3983e6631feb370051a708f3751
Marcelo Migotto <marcelo.dewiz@gmail.com>
2024-02-10 01:53:45 -0300

    Aula de persistencia de dados

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from galeria.models import Fotografia
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id", "nome", "legenda", "publicada")
+    list_display_links = ("id", "nome",)
+    search_fields = ("nome",)
+    list_filter = ("categoria",)
+    list_editable = ("publicada", )
+    list_per_page = 1
+
+admin.site.register(Fotografia, ListandoFotografias)
